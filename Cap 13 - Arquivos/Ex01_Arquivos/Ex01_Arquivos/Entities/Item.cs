446df54083a638b9b82c970594aa00fc29795602
Thiago Turcato do Rego <turcato1@uol.com.br>
2020-12-20 21:25:55 -0300

    Curso de C# até o capítulo 13

---
+++ Cap 13 - Arquivos/Ex01_Arquivos/Ex01_Arquivos/Entities/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex01_Arquivos.Entities
+{
+    class Item
+    {
+        public string ProdName { get; set; }
+        public double UnitValue { get; set; }
+        public int Quantity { get; set; }
+
+        public Item(string prodName, double unitValue, int quantity)
+        {
+            ProdName = prodName;
+            UnitValue = unitValue;
+            Quantity = quantity;
+        }
+
+        public double Subtotal()
+        {
+            return UnitValue * Quantity;
+        }
+    }
+}
---
+++ Cap 13 - Arquivos/Ex01_Arquivos/Ex01_Arquivos/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
+using Ex01_Arquivos.Entities;
+
+namespace Ex01_Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFilePath = @"..\..\..\Files\List.csv";
+            string destinationPath = @"..\..\..\Files\out";
+            List<Item> SourceItems = new List<Item>();
+
+            try
+            {
+                Console.WriteLine("Reading source file " + Path.GetFullPath(sourceFilePath) + " ...");
+                string[] sourceFileLines = File.ReadAllLines(sourceFilePath);
+                Console.WriteLine("Processing file data... ");
+                foreach (string line in sourceFileLines)
+                {
+                    string[] lineVector = line.Split(",");
+                    string lineItemName = lineVector[0];
+                    double lineItemPrice = double.Parse(lineVector[1], CultureInfo.InvariantCulture);
+                    int lineItemQuantity = int.Parse(lineVector[2]);
+                    SourceItems.Add(new Item(lineItemName, lineItemPrice, lineItemQuantity));
+                }
+                try
+                {
+                    Console.WriteLine("Creating subdirectory 'out'... ");
+                    Directory.CreateDirectory(destinationPath);
+
+                    Console.WriteLine(@"Creating output file out\summary.csv... ");
+                    using (FileStream fs = new FileStream(destinationPath + @"\summary.csv", FileMode.Create)) 
+                    {
+                        using (StreamWriter sw = new StreamWriter(fs))
+                        {
+                            Console.WriteLine("Writing data in output file... ");
+                            foreach (Item item in SourceItems)
+                            {
+                                sw.WriteLine(item.ProdName + "," + item.Subtotal().ToString("F2", CultureInfo.InvariantCulture));
+                            }
+                        }
+                    }
+                    Console.WriteLine("Output file complete");
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Error writing output file: " + e.Message);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error in reading file: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error in file data: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
+        }
+    }
+}
